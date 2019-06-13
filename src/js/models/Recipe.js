70aa3fce3d9116dac2638a842e0b757b3f0364e3
Nedim Kulovac <nedim.kulovac@gmail.com>
2019-06-13 15:57:49 +0200

    Recepe rendering to UI implemented

--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -36,6 +36,7 @@ export default class Recipe {
     parseIngredients() {
         const unitLong = ['tablespoons', 'tablespoon', 'ounces', 'ounce', 'teaspoons', 'teaspoon', 'cups', 'pounds'];
         const unitShort = ['tbsp', 'tbsp', 'oz', 'oz', 'tsp', 'tsp', 'cup', 'pound'];
+        const units = [...unitShort, 'kg', 'g'];
 
         const newIngredients = this.ingredients.map(el => {
             // 1. uniform units
@@ -49,7 +50,7 @@ export default class Recipe {
 
             // 3. Parse ingredients into CountQueuingStrategy, unit and ingredient
             const arrIng = ingredient.split(' ');
-            const unitIndex = arrIng.findIndex(el2 => unitShort.includes(el2));
+            const unitIndex = arrIng.findIndex(el2 => units.includes(el2));
 
             let objIng;
             if (unitIndex > -1) {
