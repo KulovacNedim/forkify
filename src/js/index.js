bcd4ad6c1e2b7d693d5a74f9c9da821a66f79798
Nedim Kulovac <nedim.kulovac@gmail.com>
2019-06-13 11:45:19 +0200

    Search model and search controller basic made

--- src/js/index.js
+++ src/js/index.js
@@ -14,7 +14,7 @@ const controlSearch = async () => {
     // 1. Get query from view
     const query = searchView.getInput();
 
-    if(query) {
+    if (query) {
         // 2. New search object and add to state
         state.search = new Search(query);
 
@@ -23,12 +23,17 @@ const controlSearch = async () => {
         searchView.clearResults();
         renderLoader(elements.searchRes);
 
-        // 4. Search for recipes
-        await state.search.getResults();
+        try {
+            // 4. Search for recipes
+            await state.search.getResults();
 
-        // 5. Redned results on UI
-        clearLoader();
-        searchView.renderResults(state.search.result);
+            // 5. Redned results on UI
+            clearLoader();
+            searchView.renderResults(state.search.result);
+        } catch (error) {
+            alert('Something wrong with search...');
+            clearLoader();
+        }
     }
 }
 
@@ -47,6 +52,32 @@ elements.searchResPages.addEventListener('click', e => {
 });
 
 // RECIPE CONTROLER
-const r = new Recipe(46956);
-r.getRecipe();
-console.log(r)
+const controlRecipe = async () => {
+    // Get ID from url
+    const id = window.location.hash.replace('#', '');
+    console.log(id);
+
+    if (id) {
+        // Prepare UI for changes
+
+        // Create new recipe object
+        state.recipe = new Recipe(id);
+
+        try {
+            // Get recipe data
+            await state.recipe.getRecipe();
+
+            // Calculate serving and time
+            state.recipe.calcTime();
+            state.recipe.calcServings();
+
+            // Render recipe
+            console.log(state.recipe)
+        } catch (error) {
+            alert('Error processing recipe!')
+        }
+
+    }
+};
+
+['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -10,7 +10,7 @@ export default class Search {
     async getResults() {
         try {
             const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
-            this.result = res.data.recipes;console.log(this.result)
+            this.result = res.data.recipes;
         } catch (error) {
             alert(error);
         }
